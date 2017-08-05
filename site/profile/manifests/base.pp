244f203f3f23bf9e217707daed61ae3b29762d56
Hedi Nasr <h.nsr69@gmail.com>
2017-08-05 16:36:27 +0200

    add some tests

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,20 +1,25 @@
 # profile/manifests/base.pp
 class profile::base {
 
-  include ntp
-  include apt
+  if $::osfamily == 'Debian' {
+    include ntp
+    include apt
 
-  class { 'motd':
-    content => "This is host ${::fqdn}.",
+    class { 'motd':
+      content => "This is host ${::fqdn}.",
+    }
+
+    create_resources('user', hiera_hash('user', {}))
+    create_resources('group', hiera_hash('group', {}))
+    create_resources('ssh_authorized_key', hiera_hash('ssh_authorized_keys', {}))
+
+    $enhancers = [ 'vim', 'tmux', 'htop', 'mlocate', 'lsb-release' ] # todo -> run updatedb after install mlocate
+    package { $enhancers:
+      ensure  => 'installed',
+      require => Exec['apt_update'],
+    }
   }
-
-  create_resources('user', hiera_hash('user', {}))
-  create_resources('group', hiera_hash('group', {}))
-  create_resources('ssh_authorized_key', hiera_hash('ssh_authorized_keys', {}))
-
-  $enhancers = [ 'tmux', 'htop', 'mlocate', 'lsb-release' ] # todo -> run updatedb after install mlocate
-  package { $enhancers:
-    ensure  => 'installed',
-    require => Exec['apt_update'],
+  else {
+    warning ('os not (yet) supported!')
   }
 }
--- spec/acceptance/profile_spec.rb
+++ spec/acceptance/profile_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper_acceptance'
 
+packages = [
+  'vim',
+  'tmux',
+  'htop',
+  'ntp',
+  'lsb-release']
+
 describe 'profile' do
   let(:manifest) {
     <<-EOS
@@ -14,5 +21,16 @@ EOS
   it 'should apply a second time without changes' do
     @result = apply_manifest(manifest)
     expect(@result.exit_code).to be_zero
+  end
+
+  packages.each do |p|
+    describe package(p), :if => os[:family] == 'debian' do
+      it { should be_installed }
+    end
+  end
+
+  describe service('ntp'), :if => os[:family] == 'debian' do
+    it { should be_running }
+    it { should be_enabled }
   end
 end
